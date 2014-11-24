349b92d61af9d786e580aa81c6be341423cb9991
Max Heisinger <mail@maximaximal.com>
2014-11-24 23:23:02 +0100

    Added first test source file.

---
+++ compile.bat
@@ -0,0 +1,15 @@
+::@echo off
+
+set tibasic=%1
+
+call "%tibasic%" "%~dp0\TokenIDE\main.txt"
+
+move /Y "%~dp0\TokenIDE\main.8xp" "%~dp0\chemsuite.8xp"
+
+IF "%2"=="start" (
+	SET TILEM=%3
+	SET ROM=%4
+
+    ::Starting the emulator!
+    call %3 -r %4 %~dp0\chemsuite.8xp
+)
